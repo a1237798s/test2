e74afced4870740eb7f7f4ed56a1fe398c1b53b4
a1237798s <raiter0010304@gmail.com>
2025-01-03 13:31:00 +0800

    新增Class

---
+++ slnWebApplication1/WebApplication1/Models/AClass.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class AClass
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+    }
+}
--- slnWebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ slnWebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f77ee9228be0cf09dea6afcb04c0b4376808269e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94127fbc2429370cb735c077d96fcc1c54cfffc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
